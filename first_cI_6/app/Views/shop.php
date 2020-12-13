6f8875656415246085a4dcb6896fc455a80cdb9f
Roger De Four <radfinancialsystems@gmail.com>
2020-12-13 08:00:06 -0400

    Video 6 - 'Views' going to be studied fromm this mornining

---
+++ first_cI_6/app/Views/shop.php
@@ -0,0 +1,16 @@
+<!doctype html lang="en-TT">
+
+<html>
+
+<head>
+        <h2>controller learning for CI4</h2>
+
+        <p>This is a shop</p>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
---
+++ first_ci_5/app/Controllers/Admin/Users.php
@@ -0,0 +1,17 @@
+<?php namespace App\Controllers;
+
+class Userr extends BaseController
+{
+	public function index()
+	{
+		echo '<h2>This is your user area</h2>'; 
+    }
+    
+    public function getAllUsers ()
+	{
+       echo '<h2>Show all users </h2>'
+	}
+
+	//--------------------------------------------------------------------
+
+}
